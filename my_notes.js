44916956aae4ef625d868cb6b338538560906096
Lisa Vogt <lisavogtsf@users.noreply.github.com>
2014-09-16 02:56:49 -0700

    Update my_notes.js

--- my_notes.js
+++ my_notes.js
@@ -1,2 +1,4 @@
       it("returns an empty array when there are no completions", function(){
         expect(t.autoComplete("a")).toEqual([]);
+We get back an empty array from words function if there are no words in the trie. 
+So somehow we need to set it up so that the passing in a try with no words in it if we're trying to complete something and we don't have any words like that.
