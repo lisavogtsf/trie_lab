ed1031bde61324be1c101fb200ecde49d73f77af
Lisa Vogt <lisavogtsf@users.noreply.github.com>
2014-09-16 03:24:35 -0700

    Update my_notes.js

--- my_notes.js
+++ my_notes.js
@@ -10,12 +10,16 @@ So maybe you first called find on whatever you're given as auto complete, then f
 
 Trie.find("a") returns false,
 So 
-if (Trie.find("a")){
+if (Trie.find("a"/prefix)){
   Other case
+  AllWordsInTrie = trie.GetWords
+  
 } else {
   Var empty = new Trie
   Return GetWords(empty)
 }
+
+Autocomplete gets a prefix parameter. If trie has prefix & no children of that word, return prefix, return GetWords of a trie containing the prefix but no siblings. Return beast but not be. Used find to determine the word is there. Test GetWords result array?  Get words on learned/on trie = be! begin, beginner, beast. Rule out words/tries? That don't contain prefix. 
 
 
 
@@ -27,3 +31,12 @@ and run autocompleter.readFile("./mini_titles").
 There needs to be some kind of method called to read file which calls learn on whatever it's parameters are. 
 So it looks like auto completer must be a class, or rather a constructor.
 Maybe not, we have already created the try as a constructor, Andra, and auto complete as a critter method on it, also in the tests they separately run the learning method before they run auto complete, so maybe we don't have to include learn in it.
+ 
+   describe(".autoComplete", function(){
+      beforeEach(function(){
+        t.learn("be");
+        t.learn("begin");
+        t.learn("beginner");
+        t.learn("beast");
+      });
+
